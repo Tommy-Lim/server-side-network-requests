116481292c640ab54e6c1ffa3cb0322adc4a624e
Steve Geluso <stevegeluso@gmail.com>
2016-12-22 12:46:35 -0800

    adding search form

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  request('http://www.omdbapi.com/?s=star+wars', function (error, response, body) {
+  var baseUrl = 'http://www.omdbapi.com/';
+  var query = 'star wars';
+
+  // allow people to search using query parameters.
+  if (req.query.q !== undefined) {
+    query = req.query.q;
+  }
+
+  // attach the query to the url.
+  var url = baseUrl + '?s=' + query;
+
+  request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var results = JSON.parse(body).Search;
 
